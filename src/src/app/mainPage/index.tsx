737cef591bfdff848ce2827dfba31b15af8960e1
Сергей Краснолуцкий <vip.41243@gmail.com>
2020-03-14 23:30:28 +0700

    add moc to mainPage

--- src/src/app/mainPage/index.tsx
+++ src/src/app/mainPage/index.tsx
@@ -4,6 +4,47 @@ import { Container } from '@material-ui/core';
 import { Search } from '../../components/search';
 import { DetailFilm } from '../../components/detailFilm';
 
+const MocCurrentFilm = {
+    Title: 'V for Vendetta',
+    Year: '2005',
+    Rated: 'R',
+    Released: '17 Mar 2006',
+    Runtime: '132 min',
+    Genre: 'Action, Drama, Sci-Fi, Thriller',
+    Director: 'James McTeigue',
+    Writer: 'Lilly Wachowski (screenplay), Lana Wachowski (screenplay), David Lloyd (graphic novel art)',
+    Actors: 'Natalie Portman, Hugo Weaving, Stephen Rea, Stephen Fry',
+    Plot: 'Tells the story of Evey Hammond and her unlikely but instrumental part in bringing down the fascist government that has taken control of a futuristic Great Britain. Saved from a life-and-death situation by a man in a Guy Fawkes mask who calls himself V, she learns a general summary of V\'s past and, after a time, decides to help him bring down those who committed the atrocities that led to Britain being in the shape that it is in.',
+    Language: 'English',
+    Country: 'USA, UK, Germany',
+    Awards: '7 wins & 27 nominations.',
+    Poster: 'https://m.media-amazon.com/images/M/MV5BOTI5ODc3NzExNV5BMl5BanBnXkFtZTcwNzYxNzQzMw@@._V1_SX300.jpg',
+    Ratings: [
+        {
+            Source: 'Internet Movie Database',
+            Value: '8.2/10'
+        },
+        {
+            Source: 'Rotten Tomatoes',
+            Value: '73%'
+        },
+        {
+            Source: 'Metacritic',
+            Value: '62/100'
+        }
+    ],
+    Metascore: '62',
+    imdbRating: '8.2',
+    imdbVotes: '981,704',
+    imdbID: 'tt0434409',
+    Type: 'movie',
+    DVD: '01 Aug 2006',
+    BoxOffice: '$70,500,000',
+    Production: 'Warner Bros. Pictures',
+    Website: 'N/A',
+    Response: 'True'
+};
+
 export class MainPage extends Component{
     render(): ReactElement {
         return (
@@ -14,7 +55,7 @@ export class MainPage extends Component{
                     justify="flex-start"
                     alignItems="center">
                     <Search/>
-                    <DetailFilm/>
+                    <DetailFilm data={MocCurrentFilm}/>
                 </Grid>
             </Container>
         );
--- src/src/components/detailFilm/index.tsx
+++ src/src/components/detailFilm/index.tsx
@@ -1,7 +1,38 @@
 import React, { Component,ReactElement } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid'
+import './common.css';
 
-export class DetailFilm extends Component {
+interface Props {
+    // дописать интерфейс даты
+    data: any;
+}
+
+export class DetailFilm extends Component<Props> {
     render(): ReactElement {
-        return <span>DetailFilm</span>;
+        const { data } = this.props;
+
+        return (
+            <div className="detailFilm">
+                <Paper className="detailFilm__content">
+                    <Grid container spacing={3}>
+                        <Grid item xs={4}>
+                            <img className="detailFilm__poster" src={data.Poster} alt=""/>
+                        </Grid>
+                        <Grid item xs={8} alignContent="flex-start">
+                            <div className="detailFilm__header">
+                                <h1 className="detailFilm__title">
+                                    {data.Title}
+                                </h1>
+                            </div>
+
+                            <p className="detailFilm__plot">
+                                {data.Plot}
+                            </p>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </div>
+        );
     }
 }
