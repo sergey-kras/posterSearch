e7f2a211d8fbebc4155baa4dea28f6eee766cfa7
Сергей Краснолуцкий <vip.41243@gmail.com>
2020-03-14 22:50:15 +0700

    add base page struct and base components

--- src/src/app/mainPage/index.tsx
+++ src/src/app/mainPage/index.tsx
@@ -1,9 +1,22 @@
 import React, { Component, ReactElement } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+import { Search } from '../../components/search';
+import { DetailFilm } from '../../components/detailFilm';
 
 export class MainPage extends Component{
     render(): ReactElement {
-        return (<div>
-            some
-        </div>);
+        return (
+            <Container maxWidth="md">
+                <Grid
+                    container
+                    direction="column"
+                    justify="flex-start"
+                    alignItems="center">
+                    <Search/>
+                    <DetailFilm/>
+                </Grid>
+            </Container>
+        );
     }
 }
---
+++ src/src/components/detailFilm/index.tsx
@@ -0,0 +1,7 @@
+import React, { Component,ReactElement } from 'react';
+
+export class DetailFilm extends Component {
+    render(): ReactElement {
+        return <span>DetailFilm</span>;
+    }
+}
