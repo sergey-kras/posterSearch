ca988c588a75a8afb3b61af7188cf24310c06ae0
Сергей Краснолуцкий <vip.41243@gmail.com>
2020-03-14 21:57:25 +0700

    init

---
+++ src/src/app/mainPage/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component, ReactElement } from 'react';
+
+export class MainPage extends Component{
+    render(): ReactElement {
+        return (<div>
+            some
+        </div>);
+    }
+}
---
+++ src/src/components/search/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function f() {
+    return <span>
+        some
+    </span>;
+}
